a6fdd11d0191d8e95f869443d5a1ac2dba0a0742
Carlos Garcia <111895268+Carlos-CGS@users.noreply.github.com>
2023-12-04 12:30:19 -0300

    Add files via upload

---
+++ pianoTech/src/scripts/engineP.js
@@ -0,0 +1,38 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
+
+let mapedKeys = [];
+
+let audio = new Audio("src/tunes/a.wav");
+const playTune = (key)=>{
+    audio.src = `src/tunes/${key}.wav`;
+    audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(()=> {
+        clickedKey.classList.remove("active");
+    },150);
+};
+
+pianoKeys.forEach((key)=>{
+    key.addEventListener("click", ()=> playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
+});
+
+document.addEventListener("keydown", (e) => {
+    if (mapedKeys.includes(e.key)){
+        playTune(e.key);   
+    }
+});
+
+const handleVolume = (e)=>{
+    audio.volume = e.target.value;
+};
+volumeSlider.addEventListener("input", handleVolume);
+
+const showHideKeys = ()=>{
+    pianoKeys.forEach((key) =>key.classList.toggle("hide"));
+}
+keysCheck.addEventListener("click", showHideKeys);
